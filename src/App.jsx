8c77e62e2838b2b81fd236fe0a680a3f3abe0653
Parsa <149250117+parsa-vesali@users.noreply.github.com>
2024-06-06 01:16:53 +0330

    project updated

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,8 @@ import routes from './routes'
 export default function App() {
   let router = useRoutes(routes)
   return (
-    <>
+    <div className=' font-Dana'>
       {router}
-    </>
+    </div>
   )
 }
---
+++ src/Constant/index.js
@@ -0,0 +1,173 @@
+export const menuCategory = [
+    {
+        id: 1,
+        title: 'برنامه نویسی',
+        category: 'programming',
+        subMenu: [
+            {
+                id: 1,
+                title: 'زبان ها',
+                subMenu: [
+                    { id: 1, title: 'payton' },
+                    { id: 2, title: 'javaScript' },
+                    { id: 3, title: 'C#' },
+                ]
+            },
+            {
+                id: 2,
+                title: 'فریم ورک ها',
+                subMenu: [
+                    { id: 1, title: 'React.js' },
+                    { id: 2, title: 'Flutter' },
+                    { id: 3, title: 'Node.js' },
+                ]
+            },
+            {
+                id: 3,
+                title: 'دیتا بیس',
+                subMenu: [
+                    { id: 1, title: 'Data' },
+                    { id: 2, title: 'My Sql' },
+                    { id: 3, title: 'Sql server' },
+                ]
+            },
+            {
+                id: 4,
+                title: 'هوش مصونی',
+                subMenu: [
+                    { id: 1, title: 'Machine Learning' },
+                    { id: 2, title: 'Deep Learning' },
+                    { id: 3, title: 'Reinforcement' },
+                ]
+            },
+            {
+                id: 5,
+                title: 'شبکه',
+                subMenu: [
+                    { id: 1, title: 'network+' },
+                    { id: 2, title: 'network' },
+                    { id: 3, title: 'A+' },
+                ]
+            },
+        ]
+    },
+    {
+        id: 2,
+        title: 'ریاضی',
+        category: 'mathematics',
+        subMenu: [
+            {
+                id: 1,
+                title: 'جبر و هندسه',
+                subMenu: [
+                    { id: 1, title: 'جبر' },
+                    { id: 2, title: 'هندسه' },
+                ]
+            },
+            {
+                id: 2,
+                title: 'دیفرانسیل',
+                subMenu: [
+                    { id: 1, title: 'دیفرانسیل ساده' },
+                    { id: 2, title: 'دیفرانسیل جزئی' },
+                ]
+            },
+            {
+                id: 3,
+                title: 'حسابداری',
+                subMenu: [
+                    { id: 1, title: 'مقدماتی' },
+                    { id: 2, title: 'پیشرفته' },
+                ]
+            },
+            
+            {
+                id: 4,
+                title: 'آنالیز',
+                subMenu: [
+                    { id: 1, title: 'آنالیز عددی' },
+                    { id: 2, title: 'آنالیز واقعی' },
+                ]
+            },
+           
+        ]
+    },
+    {
+        id: 3,
+        title: 'مهندسی',
+        category: 'engineering',
+        subMenu: [
+            {
+                id: 1,
+                title: 'مکانیک',
+                subMenu: [
+                    { id: 1, title: 'مکانیک سیالات' },
+                    { id: 2, title: 'مکانیک جامدات' },
+                ]
+            },
+            {
+                id: 2,
+                title: 'برق',
+                subMenu: [
+                    { id: 1, title: 'مدارهای الکتریکی' },
+                    { id: 2, title: 'سیستم‌های قدرت' },
+                ]
+            },
+            {
+                id: 3,
+                title: 'کامپیوتر',
+                subMenu: [
+                    { id: 1, title: 'سخت‌افزار' },
+                    { id: 2, title: 'نرم‌افزار' },
+                ]
+            },
+        ]
+    },
+    {
+        id: 4,
+        title: 'مدیریت',
+        category: 'management',
+        subMenu: [
+           
+        ]
+    },
+    {
+        id: 5,
+        title: 'پزشکی',
+        category: 'medical',
+        subMenu: [
+            {
+                id: 1,
+                title: 'آناتومی',
+                subMenu: [
+                    { id: 1, title: 'آناتومی عمومی' },
+                    { id: 2, title: 'آناتومی اختصاصی' },
+                ]
+            },
+            {
+                id: 2,
+                title: 'داروسازی',
+                subMenu: [
+                    { id: 1, title: 'فارماکولوژی' },
+                    { id: 2, title: 'داروشناسی' },
+                ]
+            },
+        ]
+    },
+    {
+        id: 6,
+        title: 'فیزیک',
+        category: 'physics',
+        subMenu: []
+    },
+    {
+        id: 7,
+        title: 'زبان‌های خارجی',
+        category: 'languages',
+        subMenu: []
+    },
+];
+
+
+
+
