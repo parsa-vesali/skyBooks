d27be77f6cb4a6fac1d85b943e9914b833e75cc1
Parsa <149250117+parsa-vesali@users.noreply.github.com>
2024-06-08 13:01:59 +0330

    project updated

--- src/Components/AccounteBox.jsx
+++ src/Components/AccounteBox.jsx
@@ -8,7 +8,7 @@ export default function AccounteBox() {
             <CiUser className='text-xl font-bold' />
             <span>حساب کاربری</span>
             <div className='absolute w-[250px] bg-white top-20 border-t-2 border-gray-200 shadow-xl flex flex-col gap-y-2 child:text-sm text-gray-500  rounded-lg transition-all delay-100 invisible opacity-0 group-hover:top-14 group-hover:opacity-100 group-hover:visible py-3 px-2 z-50'>
-                <div className="px-4">
+                <div className="px-4 flex flex-col">
                     <a href="#">نام: پارسا وصالی</a>
                     <span>موبایل: 09920087317</span>
                 </div>
--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
 
 export default function SearchBar() {
+    
     return (
-        <div className="relative flex justify-between p-1 bg-zinc-100 rounded-lg w-[400px] shadow">
-            <input type="text" className='px-2 outline-none border-none bg-transparent' placeholder='جستجو کنید...' />
-            <span className='w-8 h-8 flex items-center justify-center bg-rose-500 hover:bg-rose-400 transition-all cursor-pointer rounded-lg text-white text-sm'>
+        <div  className=" relative flex justify-end p-1.5 bg-zinc-100 rounded-lg w-[450px] ">
+            <input type="text" className='absolute top-0 left-0 right-0 bottom-0 w-full px-2 rounded-lg  outline-none border-none bg-transparent text-sm   focus:ring focus:ring-sky-300  z-10' placeholder='جستجو کنید...' />
+            <span className='w-8 h-8 flex items-center justify-center bg-rose-500 hover:bg-rose-400 transition-all cursor-pointer rounded-lg text-white text-sm z-20'>
                 <SearchOutlinedIcon />
             </span>
         </div>
